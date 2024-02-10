791a8065fe146bc88c0229e0b3c9e4790fb78267
slemanz <william_sleman@hotmail.com>
2024-02-10 16:15:14 -0300

    project(feat): folder ideas

---
+++ 00_IDEAS/x_slider.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.25)
+
+t = np.arange(0.0, 100.0, 0.1)
+# s = np.sin(2*np.pi*t)
+s = 0.1*t
+l, = plt.plot(t,s)
+plt.axis([0, 10, 0, 12])
+
+axcolor = 'lightgoldenrodyellow'
+axpos = plt.axes([0.2, 0.1, 0.65, 0.03], facecolor=axcolor)
+
+spos = Slider(axpos, 'Pos', 0.1, 90.0)
+
+def update(val):
+    pos = spos.val
+    ax.axis([pos,pos+10,0,12])
+    fig.canvas.draw_idle()
+
+spos.on_changed(update)
+
+plt.show()
