99e32d722693fc2e784774aee995ed421fbf2401
slemanz <william_sleman@hotmail.com>
2024-03-25 03:42:03 -0300

    01(feat): add text in subplot

--- 01_ONE_DIMENSION_AIRPLANE/straight_line.py
+++ 01_ONE_DIMENSION_AIRPLANE/straight_line.py
@@ -35,7 +35,11 @@ def update_plot(num):
     plane_4.set_data([x[num]-30,x[num]-40],[y[num],y[num] - 0.15])
     plane_5.set_data([x[num]-30,x[num]-40],[y[num],y[num] + 0.15])
 
-    return plane_trajectory, plane_1,plane_2, plane_3, plane_4, plane_5
+    stopwatch0.set_text(str(round(t[num], 1)) + ' hrs')
+    dist_counter0.set_text(str(int(x[num])) + ' km')
+
+    return plane_trajectory, plane_1,plane_2, plane_3, plane_4, plane_5,\
+    stopwatch0
 
 
 fig = plt.figure(figsize=[16,9], dpi=120, facecolor=(0.8,0.8,0.8))
@@ -61,13 +65,23 @@ house_3,=ax0.plot([700,700],[0,0.7], 'k', linewidth=15)
 house_4,=ax0.plot([900,900],[0,1.0], 'k', linewidth=10)
 house_5,=ax0.plot([1300,1300],[0,1.0], 'k', linewidth=20)
 
+box_object = dict(boxstyle='square', fc=(0.9,0.9,0.9), ec='g', lw=5)
+stopwatch0 = ax0.text(1400, 0.65, '', size=20, color='g', bbox=box_object)
+
+box_object2 = dict(boxstyle='square', fc=(0.9,0.9,0.9), ec='g', lw=5)
+dist_counter0 = ax0.text(1000, 0.65, '', size=20, color='g', bbox=box_object2)
 
 
+# subplot properties
 plt.xlim(x[0], x[-1])
 plt.ylim(0, y[-1]+1)
-
 plt.xticks(np.arange(x[0], x[-1] + 1, x[-1]/4), size=15) # cut in 4 pieces the x
 plt.yticks(np.arange(0,y[-1]+2, y[-1]/y[-1]), size=15) # (0, 2+2, 2/2)
+plt.xlabel('x-distance', fontsize=15)
+plt.ylabel('y-distance', fontsize=15)
+plt.title('Airplane', fontsize=20)
+plt.grid(True)
+
 
 
 
