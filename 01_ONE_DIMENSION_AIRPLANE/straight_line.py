733d1e0aaf214522236c5fc6d424284acbd67ff1
slemanz <william_sleman@hotmail.com>
2024-03-30 19:29:24 -0300

    01(feat): begin 3rd plot

--- 01_ONE_DIMENSION_AIRPLANE/straight_line.py
+++ 01_ONE_DIMENSION_AIRPLANE/straight_line.py
@@ -37,6 +37,7 @@ for i in range(0, frame_amount):
         dot[i] = x[n-20]
 
 
+
 def update_plot(num):
 
     plane_trajectory.set_data(dot[0:num], y[0:num]) 
@@ -45,6 +46,7 @@ def update_plot(num):
     plane_3.set_data([x[num],x[num] -20],[y[num],y[num] - 0.3])
     plane_4.set_data([x[num]-30,x[num]-40],[y[num],y[num] - 0.15])
     plane_5.set_data([x[num]-30,x[num]-40],[y[num],y[num] + 0.15])
+
     plane_vertical.set_data([x[num],x[num]], [0, y[num]])
 
     stopwatch0.set_text(str(round(t[num], 1)) + ' hrs')
@@ -85,21 +87,21 @@ house_3,=ax0.plot([700,700],[0,0.7], 'k', linewidth=15)
 house_4,=ax0.plot([900,900],[0,1.0], 'k', linewidth=10)
 house_5,=ax0.plot([1300,1300],[0,1.0], 'k', linewidth=20)
 
-box_object = dict(boxstyle='square', fc=(0.9,0.9,0.9), ec='g', lw=5)
-stopwatch0 = ax0.text(1400, 0.65, '', size=20, color='g', bbox=box_object)
+box_object = dict(boxstyle='square', fc=(0.9,0.9,0.9), ec='g', lw=2)
+stopwatch0 = ax0.text(1400, 0.75, '', size=14, color='g', bbox=box_object)
 
-box_object2 = dict(boxstyle='square', fc=(0.9,0.9,0.9), ec='g', lw=5)
-dist_counter0 = ax0.text(1000, 0.65, '', size=20, color='g', bbox=box_object2)
+box_object2 = dict(boxstyle='square', fc=(0.9,0.9,0.9), ec='r', lw=2)
+dist_counter0 = ax0.text(1400, 0.25, '', size=14, color='r', bbox=box_object2)
 
 
 # subplot properties
 plt.xlim(x[0], x[-1])
 plt.ylim(0, y[-1]+1)
-plt.xticks(np.arange(x[0], x[-1] + 1, x[-1]/4), size=15) # cut in 4 pieces the x
-plt.yticks(np.arange(0,y[-1]+2, y[-1]/y[-1]), size=15) # (0, 2+2, 2/2)
-plt.xlabel('x-distance', fontsize=15)
-plt.ylabel('y-distance', fontsize=15)
-plt.title('Airplane', fontsize=20)
+plt.xticks(np.arange(x[0], x[-1] + 1, x[-1]/4), size=12) # cut in 4 pieces the x
+plt.yticks(np.arange(0,y[-1]+2, y[-1]/y[-1]), size=12) # (0, 2+2, 2/2)
+plt.xlabel('x-distance', fontsize=12)
+plt.ylabel('y-distance', fontsize=12)
+plt.title('Airplane', fontsize=16)
 plt.grid(True)
 
 
@@ -107,21 +109,31 @@ plt.grid(True)
 
 ax2=fig.add_subplot(gs[1,0], facecolor=(0.9,0.9,0.9))
 x_dist,=ax2.plot([],[], '-b', linewidth=3, label='x=800*t')
-horizontal_line,=ax2.plot([],[],'r:o', linewidth=2, label='horizontal line')
-vertical_line,=ax2.plot([],[],'g:o', linewidth=2, label='vertical line')
+horizontal_line,=ax2.plot([],[],'r:o', linewidth=2, label='distance covered')
+vertical_line,=ax2.plot([],[],'g:o', linewidth=2, label='time spent')
 
 plt.xlim(t[0], t[-1])
 plt.ylim(x[0], x[-1])
-plt.xticks(np.arange(t[0], t[-1] + dt, t[-1]/4), size=15) 
-plt.yticks(np.arange(x[0], x[-1] + 1, x[-1]/4), size=15) 
-plt.xlabel('time [hrs]', fontsize=15)
-plt.ylabel('x-distance [km]', fontsize=15)
+plt.xticks(np.arange(t[0], t[-1] + dt, t[-1]/4), size=10) 
+plt.yticks(np.arange(x[0], x[-1] + 1, x[-1]/4), size=10) 
+plt.xlabel('time [hrs]', fontsize=10)
+plt.ylabel('x-distance [km]', fontsize=10)
 plt.grid(True)
-plt.legend(loc='upper left', fontsize='x-large')
+plt.legend(loc='upper left', fontsize=10)
 
+# subplot 3
 
+ax3=fig.add_subplot(gs[1,1], facecolor=(0.9,0.9,0.9))
+speed_line,=ax2.plot([],[], '-b', linewidth=3, label="FUNCTION: ")
+vertical_line2,=ax2.plot([],[],'b:o', linewidth=2, label='speed')
 
+plt.xlim(t[0], t[-1])
+plt.ylim(x[0], x[-1])
+plt.xticks(np.arange(t[0], t[-1] + dt, t[-1]/4), size=10) 
+plt.yticks(np.arange(x[0], x[-1] + 1, x[-1]/4), size=10) 
 
+plt.xlabel('time [hrs]', fontsize=10)
+plt.ylabel('speed [km/h]', fontsize=10)
 
 
 
